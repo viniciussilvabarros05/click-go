18bcde1dbbfbd3131ed316311e0c798a2a4cf4e5
viniciussilvabarros05 <viniciussilvabarros05@hotmail.com>
2024-02-24 11:20:30 -0300

    feat: add cardService component

--- src/app/components/cardService.tsx
+++ src/app/components/cardService.tsx
@@ -5,8 +5,8 @@ interface CardServiceProps{
 const CardService = ({children}: CardServiceProps) => {
   return (
     <div className="flex items-center justify-center rounded-md">
-      <div className="h-[300px] w-[540px] rounded-[4px] bg-gradient-to-r to-primary via-secondary from-[#ff9102] p-[3px]">
-        <div className="flex h-full w-full items-start justify-start bg-[#171717] back p-8 flex-col gap-4">
+      <div className="h-[340px] w-[550px] rounded-[4px] bg-gradient-to-r to-primary via-secondary from-[#ff9102] p-[3px]">
+        <div className="flex h-full w-full items-start justify-start bg-[#171717] back p-6 flex-col gap-4">
         {children}
         </div>
       </div>
